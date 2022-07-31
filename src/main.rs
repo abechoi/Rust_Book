ae27be8019598933f880fbb4fa30b41193ca3b70
Abe Choi <abraham.choi@icloud.com>
2022-07-31 09:40:57 -0700

    added more docs

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,22 @@ use std::io;
 
 fn main() {
 
-    // create a variable with a random u8
+    // create a random number
     let secret_number: u8 = rand::thread_rng().gen_range(1..=6);
 
     loop {
 
         println!("Enter a guess:");
 
-        // create a mutable variable for the user input as a string
+        // create a mutable string
         let mut input: String = String::new();
 
-        // read user input and handle erroneous inputs
+        // read user input
         io::stdin()
             .read_line(&mut input)
             .expect("Invalid number!");
 
-        // parse input type from a String to u8, trim whitespaces and newlines, and match the enum 
+        // parse input
         let input: u8 = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -29,6 +29,7 @@ fn main() {
 
         println!("You guessed {input}");
 
+        // compare input and string
         match input.cmp(&secret_number) {
             Ordering::Less => println!("Too low!"),
             Ordering::Greater => println!("Too high!"),
